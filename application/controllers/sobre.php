114959f10f8544d69e31aa7c85d84a2022bd7407
Tiago Pacheco Maggioni <tiago.maggioni@kriterium.com.br>
2015-04-28 15:09:53 -0300

    Sobre

---
+++ application/controllers/sobre.php
@@ -0,0 +1,25 @@
+<?php
+
+class Sobre extends CI_Controller {
+
+    function __construct() {
+        parent::__construct();
+       $this->load->helper('conversor_de_formatos');
+    }
+
+    function index() {
+        
+		$dados['title'] = "Kriativa - Sobre";
+		
+		$dados['meta'] = "<meta name='description' content='' />";
+		
+		
+
+		$this->load->view('elementos/html_header',$dados);
+        $this->load->view('sobre',$dados);
+        $this->load->view('elementos/html_footer',$dados);
+    }
+
+    
+}
+?>
