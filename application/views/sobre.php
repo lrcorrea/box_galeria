175e0d2621a1fa56a58d4643e9c11081522d949e
lucas <lucasrodriguescorrea2006@hotmail.com>
2015-04-28 16:19:06 -0300

    new about

--- application/views/sobre.php
+++ application/views/sobre.php
@@ -4,8 +4,24 @@
 
 
 <section class="conteudo">
+	<div class="bg_sobre">
+			<h1>BOX GALERIA</h1>
+			<h3>SOLUÇÃO CRIATIVA PARA AS ARTES VISUAIS</h3>
+			<h2>PROJETO BOX GALERIA LEVA EXPOSIÇOES EM<br/>FORMATO ITINERANTE A BAIRROS DE SALVADOR</h2>
+		</div>
 	<div class="centro">
-		 <h1>BOX GALERIA</h1>
 		
+		<p>A partir do mês de XXXXX de 2015, Salvador será sede de um projeto inovador. O Box Galeria vai fazer circular pela cidade exposições itinerantes que acontecerão dentro de containers, que se deslocam pelos bairros periféricos da capital. Considerando o baixo número de galerias de arte em Salvador, além do fato de elas estarem, em sua maioria, nas regiões mais centrais, o projeto busca não só a descentralização, mas a ampliação do acesso a novos artistas visuais. <br/><br/>
+
+Através de convocatória, artistas serão convidados a expor nas galerias móveis, que passarão pela curadoria dos artistas Mario Britto e Dervanier Hembadoom. A ideia é que apresentem propostas que dialoguem com o formato da exposição em contêiner, e que de alguma forma se envolva com a região na qual a galeria vai circular. Serão cinco exposições, que ficarão abertas à visitação pública durante um mês. Os bairros que recebem a galeria são X X X X X, onde também acontecerão oficinas promovidas pelos artistas em exposição, além de visitas guiadas.<br/><br/>
+
+O projeto Box Galeria apresenta uma solução criativa e viável para o gargalo na circulação de exposições de arte e na formação de público em Salvador: a adaptação de contêineres como galerias de arte, uma tendência que já possui alguns adeptos pelo mundo. Os contêineres marítimos, fora do seu uso habitual, se constituem objetos com grande capacidade de adaptação para outros fins: casas, escritórios, sanitários públicos, entre outros. O Box Galeria se apropria da tendência e utiliza contêineres para servirem como galerias móveis que circularão por bairros populares da cidade de Salvador, se instalando em um determinado local por um prazo pré-estabelecido. <br/><br/>
+
+Os equipamentos serão adaptados para receber exposições de artes plásticas, fotografias, vídeo-instalações, entre outros formatos e linguagens. Os contêineres, unidades utilizadas para o transporte, símbolo da logística e do transporte de carga, agora adquirem uma nova funcionalidade: ao mesmo tempo que transportam objetos de arte, eles também são objetos artísticos, modificados, sofrem intervenções além de ser um espaço expositivo móvel.<br/><br/>
+
+Entendendo a arte como um espaço também para a educação, as exposições pretendem estabelecer um diálogo com as comunidades que as receberão, havendo convênio com escolas da região, visitas guiadas e oficinas voltadas para os moradores dos bairros.<br/><br/> 
+O Box Galeria é um projeto financiado pela Secretaria de Cultura do Estado da Bahia, através do Edital Setorial de Artes Visuais 2014, com recursos da (qual a instituição?).
+
+</p>
 	</div>
 </section>
